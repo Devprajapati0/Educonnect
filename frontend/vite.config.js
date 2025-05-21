f25dffd3fc60744e56eca47883c5e006e0f71c74
Dev <devprajapati742@gmail.com>
2025-05-21 15:43:53 +0530

    wildcard

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // Allow external access (e.g., from a domain)
+    port: 3000,
+    allowedHosts: [/.*\.educonnect\.tech/], // Regex for wildcard subdomains
+  },
 })
